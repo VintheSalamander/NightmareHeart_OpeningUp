1ad5c9ceb65aadfe6074c3645bbbd7c64c1200c3
Jimex55 <james_dasilva@hotmail.co.uk>
2024-03-19 15:31:06 +0000

    Added End Game Scene

---
+++ Assets/HeartEndGame.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartEndGame : MonoBehaviour
+{
+
+    public GameObject gameController, gameLevel, enemies, newLevel, endCam, endPlayer, endNewPlayer, currentMusic;
+    public AudioSource rumble, endSong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == "Enemy")
+        {
+            Destroy(Other);
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        rumble.Play();
+        gameController.SetActive(false);
+        enemies.SetActive(false);
+        endPlayer.SetActive(true);
+        endCam.SetActive(true);
+        Invoke("RemoveMap", 2f);
+
+
+    }
+
+    void RemoveMap()
+    {
+        gameLevel.SetActive(false);
+        currentMusic.SetActive(false);
+        Invoke("NewMapSpawn", 2f);
+
+    }
+
+    void NewMapSpawn()
+    {
+        endSong.Play();
+        endPlayer.SetActive(false);
+        endNewPlayer.SetActive(true);
+        newLevel.SetActive(true);
+        Invoke("EndSound", 1f);
+    }
+
+    void EndSound()
+    {
+        rumble.Stop();
+    }
+}
